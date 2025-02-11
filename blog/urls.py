a577be883a54cecf74889dc699367ecd93bb169a
viego <viego@nyu.com>
2025-02-10 22:37:43 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    ## post/ means URL should begin with the word post followed by a /.
+    ## <int:pk> means Django expects an integer value and will transfer it to a view a s a variable called pk
+    ## / to finish the URL
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
